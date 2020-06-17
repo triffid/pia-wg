4986fca570314ddda7abff771b43c5bf92e836ef
Michael Moon <triffid.hunter@gmail.com>
2020-06-17 14:39:53 +0800

    Add suggestions from /u/Threarah, no longer require wg-quick, re-fetch server list after connection if cached list is stale, check that the few prerequisites are available

--- pia-wg.sh
+++ pia-wg.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 
-# original scrpt posted by tpretz at https://www.reddit.com/r/PrivateInternetAccess/comments/g08ojr/is_wireguard_available_yet/fnvs20c/
+# original script posted by tpretz at https://www.reddit.com/r/PrivateInternetAccess/comments/g08ojr/is_wireguard_available_yet/fnvs20c/
 # and at https://gist.github.com/tpretz/5ea1226517d95361f063f621e45de0a6
 #
 # significantly modified by Triffid_Hunter
 #
+# Improved with suggestions from Threarah at https://www.reddit.com/r/PrivateInternetAccess/comments/h9y4da/is_there_any_way_to_generate_wireguard_config/fv3cgi9/
+#
 # After the first run to fetch various data files and an auth token, this script does not require the ability to DNS resolve privateinternetaccess.com
+
+if ! which jq &>/dev/null
+then
+	echo "The 'jq' utility is required"
+	echo "    Most package managers should have a 'jq' package available"
+	EXIT=1
+fi
+
+if ! which ip &>/dev/null
+then
+	echo "The 'ip' utility from iproute2 is required"
+	echo "    Most package managers should have a 'iproute2' package available"
+	EXIT=1
+fi
+
+if ! which wg &>/dev/null
+then
+	echo "The 'wg' utility from wireguard/wireguard-tools is required"
+	echo "    Most package managers should have 'wireguard' or 'wireguard-tools' packages available"
+	EXIT=1
+fi
+
+[ -n "$EXIT" ] && exit 1
 
 if [ -z "$CONFIGDIR" ]
 then
@@ -108,22 +133,31 @@ fi
 # fetch data.json if missing
 if ! [ -r "$DATAFILE" ]
 then
-	echo "Fetching wireguard server list from github"
-	wget -O "$DATAFILE" 'https://raw.githubusercontent.com/pia-foss/desktop/master/tests/res/openssl/payload1/payload' || exit 1
+	echo "Fetching wireguard server list from PIA"
+	# wget -O "$DATAFILE" 'https://raw.githubusercontent.com/pia-foss/desktop/master/tests/res/openssl/payload1/payload' || exit 1
+	curl 'https://www.privateinternetaccess.com/vpninfo/servers?version=1001&client=x-alpha' > "$DATAFILE.temp" || exit 1
+	if [ "$(jq 'map_values(select(.wireguard)) | keys' "$DATAFILE.temp" 2>/dev/null | wc -l)" -le 50 ]
+	then
+		echo "Bad serverlist retrieved to $DATAFILE.temp, exiting"
+		echo "You can try again if there was a transient error"
+		exit 1
+	else
+		jq -cM 'map_values(select(.wireguard))' "$DATAFILE.temp" > "$DATAFILE" 2>/dev/null
+	fi
 fi
 
-if [ "$(jq -r ".$LOC" "$DATAFILE")" == "null" ]
+if [ "$(jq -r ".$LOC.wireguard" "$DATAFILE")" == "null" ]
 then
 	# echo "No exact match for location \"$LOC\" trying pattern"
 	# from https://unix.stackexchange.com/questions/443884/match-keys-with-regex-in-jq/443927#443927
 	LOC=$(jq 'with_entries(if (.key|test("^'"$LOC"'")) then ( {key: .key, value: .value } ) else empty end ) | keys' "$DATAFILE" | grep ^\  | cut -d\" -f2 | shuf -n 1)
 fi
 
-if [ "$(jq -r .$LOC "$DATAFILE")" == "null" ]
+if [ "$(jq -r ".$LOC.wireguard" "$DATAFILE")" == "null" ]
 then
 	echo "Location $LOC not found!"
 	echo "Options are:"
-	jq keys "$DATAFILE"
+	jq 'map_values(select(.wireguard)) | keys' "$DATAFILE"
 	echo
 	echo "Please edit $CONFIG and change your desired location, then try again"
 	exit 1
@@ -154,26 +188,27 @@ touch "$TOKENFILE"
 chmod 600 "$TOKENFILE"
 echo "$TOK" > "$TOKENFILE"
 
-WG_URL=$(jq -r ".$LOC.wireguard.host" "$DATAFILE")
-WG_SERIAL=$(jq -r ".$LOC.wireguard.serial" "$DATAFILE")
-WG_HOST=$(cut -d: -f1 <<< "$WG_URL")
-WG_PORT=$(cut -d: -f2 <<< "$WG_URL")
+WG_NAME="$(jq -r ".$LOC.name" "$DATAFILE")"
+WG_DNS="$(jq -r ".$LOC.dns" "$DATAFILE")"
+WG_URL="$(jq -r ".$LOC.wireguard.host" "$DATAFILE")"
+# WG_SERIAL="$(jq -r ".$LOC.wireguard.serial" "$DATAFILE")"
+WG_SN="$(cut -d. -f1 <<< "$WG_DNS")"
+WG_HOST="$(cut -d: -f1 <<< "$WG_URL")"
+WG_PORT="$(cut -d: -f2 <<< "$WG_URL")"
 
 if [ -z "$WG_URL" ]; then
   echo "no wg region, exiting"
   exit 1
 fi
 
-echo "Registering public key with PIA endpoint $LOC ($WG_HOST)"
+echo "Registering public key with $WG_NAME ($WG_HOST)"
 
-# should TLS verify here
-# can't verify fully as PIA's wireguard endpoint certs don't have a chain back to a root CA
-curl -GksS \
+curl -GsS \
   --data-urlencode "pubkey=$CLIENT_PUBLIC_KEY" \
   --data-urlencode "pt=$TOK" \
-  --resolve "$WG_SERIAL:$WG_PORT:$WG_HOST" \
-"https://$WG_SERIAL:$WG_PORT/addKey" > "$REMOTEINFO.temp" || (
-	echo "Failed to register key with PIA endpoint $LOC ($WG_HOST)"
+  --resolve "$WG_DNS:$WG_PORT:$WG_HOST" \
+"https://$WG_DNS:$WG_PORT/addKey" > "$REMOTEINFO.temp" || (
+	echo "Failed to register key with $WG_SN ($WG_HOST)"
 	if ip link list "$PIA_INTERFACE" > /dev/null
 	then
 		echo "If you're trying to change hosts because your link has stopped working,"
@@ -196,6 +231,7 @@ PEER_IP="$(jq -r .peer_ip "$REMOTEINFO")"
 SERVER_PUBLIC_KEY="$(jq -r .server_key  "$REMOTEINFO")"
 SERVER_IP="$(jq -r .server_ip "$REMOTEINFO")"
 SERVER_PORT="$(jq -r .server_port "$REMOTEINFO")"
+SERVER_VIP="$(jq -r .server_vip "$REMOTEINFO")"
 
 if [ -z "$WGCONF" ]
 then
@@ -226,31 +262,34 @@ if [ "$EUID" -eq 0 ]
 then
 	# scratch current config if any
 	# put new settings into existing interface instead of teardown/re-up to prevent leaks
-	if ip link list $PIA_INTERFACE > /dev/null
+	if ip link list "$PIA_INTERFACE" > /dev/null
 	then
 		echo "Updating existing interface '$PIA_INTERFACE'"
 
-		OLD_PEER_IP="$(ip -j addr show dev pia | jq '.[].addr_info[].local')"
+		OLD_PEER_IP="$(ip -j addr show dev pia | jq -r '.[].addr_info[].local')"
 		OLD_KEY="$(echo $(wg showconf "$PIA_INTERFACE" | grep ^PublicKey | cut -d= -f2-))"
 		OLD_ENDPOINT="$(wg show "$PIA_INTERFACE" endpoints | grep "$OLD_KEY" | cut -d$'\t' -f2 | cut -d: -f1)"
 
 		# ensure we don't get a packet storm loop
 		ip rule add to "$SERVER_IP" lookup china pref 10
 
-		echo "    [Change Peer from $OLD_KEY to $SERVER_PUBLIC_KEY]"
-		wg set "$PIA_INTERFACE" peer "$SERVER_PUBLIC_KEY" endpoint "$SERVER_IP:$SERVER_PORT" allowed-ips "0.0.0.0/0,::/0" || exit 1
+		if [ "$OLD_KEY" != "$SERVER_PUBLIC_KEY" ]
+		then
+			echo "    [Change Peer from $OLD_KEY to $SERVER_PUBLIC_KEY]"
+			wg set "$PIA_INTERFACE" private-key <(echo "$CLIENT_PRIVATE_KEY") peer "$SERVER_PUBLIC_KEY" endpoint "$SERVER_IP:$SERVER_PORT" allowed-ips "0.0.0.0/0,::/0" || exit 1
+			# remove old key
+			wg set "$PIA_INTERFACE" peer "$OLD_KEY" remove
+		fi
 
 		if [ "$PEER_IP" != "$OLD_PEER_IP" ]
 		then
 			echo "    [Change $PIA_INTERFACE ipaddr from $OLD_PEER_IP to $PEER_IP]"
 			# update link ip address in case
 			ip addr replace "$PEER_IP" dev "$PIA_INTERFACE"
-		fi
 
-		# remove old key
-		wg set "$PIA_INTERFACE" peer "$OLD_KEY" remove
-		# remove old route
-		ip rule del to "$OLD_PEER_IP" lookup china
+			# remove old route
+			ip rule del to "$OLD_PEER_IP" lookup china
+		fi
 	else
 		echo "Bringing up interface '$PIA_INTERFACE'"
 
@@ -258,7 +297,11 @@ then
 		ip rule add to "$SERVER_IP" lookup china
 
 		# bring up wireguard interface
-		wg-quick up "$WGCONF"
+# 		wg-quick up "$WGCONF"
+		ip link add "$PIA_INTERFACE" type wireguard || exit 1
+		ip link set dev "$PIA_INTERFACE" up || exit 1
+		wg set "$PIA_INTERFACE" private-key <(echo "$CLIENT_PRIVATE_KEY") peer "$SERVER_PUBLIC_KEY" endpoint "$SERVER_IP:$SERVER_PORT" allowed-ips "0.0.0.0/0,::/0" || exit 1
+		ip addr replace "$PEER_IP" dev "$PIA_INTERFACE" || exit 1
 
 		# Note: only if Table = off in wireguard config file above
 		ip route add default dev "$PIA_INTERFACE"
@@ -270,8 +313,17 @@ else
 	echo ip rule add to "$SERVER_IP" lookup china pref 10
 	sudo ip rule add to "$SERVER_IP" lookup china pref 10
 
-	echo wg set "$PIA_INTERFACE" peer "$SERVER_PUBLIC_KEY" endpoint "$SERVER_IP:$SERVER_PORT" allowed-ips "0.0.0.0/0,::/0"
-	sudo wg set "$PIA_INTERFACE" peer "$SERVER_PUBLIC_KEY" endpoint "$SERVER_IP:$SERVER_PORT" allowed-ips "0.0.0.0/0,::/0"
+	if ! ip link list "$PIA_INTERFACE" > /dev/null
+	then
+		echo ip link add "$PIA_INTERFACE" type wireguard
+		sudo ip link add "$PIA_INTERFACE" type wireguard
+	fi
+
+	echo wg set "$PIA_INTERFACE" private-key "$CLIENT_PRIVATE_KEY" peer "$SERVER_PUBLIC_KEY" endpoint "$SERVER_IP:$SERVER_PORT" allowed-ips "0.0.0.0/0,::/0"
+	sudo wg set "$PIA_INTERFACE" private-key <(echo "$CLIENT_PRIVATE_KEY") peer "$SERVER_PUBLIC_KEY" endpoint "$SERVER_IP:$SERVER_PORT" allowed-ips "0.0.0.0/0,::/0"
+
+	echo ip addr replace "$PEER_IP" dev "$PIA_INTERFACE"
+	sudo ip addr replace "$PEER_IP" dev "$PIA_INTERFACE"
 
 	if ip link list $PIA_INTERFACE > /dev/null
 	then
@@ -285,12 +337,27 @@ else
 		echo ip rule del to "$OLD_PEER_IP" lookup china
 		sudo ip rule del to "$OLD_PEER_IP" lookup china
 	fi
-
-	echo ip route add "$SERVER_IP" via "$GATEWAY_IP" dev "$GATEWAY_DEV"
-	sudo ip route add "$SERVER_IP" via "$GATEWAY_IP" dev "$GATEWAY_DEV"
-
-	echo wg-quick up "$WGCONF"
-	sudo wg-quick up "$WGCONF"
 fi
 
-echo "Done"
+echo "PIA Wireguard '$PIA_INTERFACE' configured successfully"
+
+echo -n "Waiting for connection to stabilise..."
+while ! ping -n -c1 -w 5 "$SERVER_VIP" &>/dev/null
+do
+	echo -n "."
+done
+echo " OK"
+
+if find "$DATAFILE" -mtime -3 -exec false {} +
+then
+	echo
+	echo "PIA endpoint cache is stale, fetching new list.."
+	curl 'https://www.privateinternetaccess.com/vpninfo/servers?version=1001&client=x-alpha' > "$DATAFILE.temp" || exit 1
+	if [ "$(jq 'map_values(select(.wireguard)) | keys' "$DATAFILE.temp" 2>/dev/null | wc -l)" -le 50 ]
+	then
+		echo "Bad serverlist retrieved to $DATAFILE.temp, ignoring"
+	else
+		jq -cM 'map_values(select(.wireguard))' "$DATAFILE.temp" > "$DATAFILE" 2>/dev/null
+		echo "Success"
+	fi
+fi
